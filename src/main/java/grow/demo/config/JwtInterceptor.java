4d286dfe0f4ffb692a83e703227b7f409d02a5b8
LeeKyuTae <crown0403@khu.ac.kr>
2019-10-15 17:20:06 +0900

    Set jwtToken authorization

---
+++ src/main/java/grow/demo/config/JwtInterceptor.java
@@ -0,0 +1,42 @@
+package grow.demo.config;
+
+
+import grow.demo.account.service.JwtService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@AllArgsConstructor
+@Component
+public class JwtInterceptor implements HandlerInterceptor {
+
+    private static final String  HEADER_AUTH = "authorization" ;
+
+
+    private final JwtService jwtService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String authorization_token =request.getHeader(HEADER_AUTH);
+        String  jwttoken = jwtService.generateToken(authorization_token);
+        if(jwttoken != null && jwtService.validateToken(jwttoken)){
+            return true;
+        }else{
+            throw new UnauthorizedException();
+        }
+    }
+
+    /*
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(HEADER_AUTH);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+     */
+}
