53addb80c5e3aeca214e2b1645e98e67cbff644a
LeeKyuTae <crown0403@khu.ac.kr>
2019-11-05 15:58:20 +0900

    Add function :register setInfo, update setInfo

--- src/main/java/grow/demo/routine/service/SetInfoService.java
+++ src/main/java/grow/demo/routine/service/SetInfoService.java
@@ -5,6 +5,7 @@ import grow.demo.account.domain.Account;
 import grow.demo.account.repository.AccountRepository;
 import grow.demo.routine.domain.exercise.Exercise;
 import grow.demo.routine.domain.routine.Routine;
+import grow.demo.routine.domain.routine.RoutineCategory;
 import grow.demo.routine.domain.routine.SetInfo;
 import grow.demo.routine.dto.SetInfoDto;
 import grow.demo.routine.repository.ExerciseRepository;
@@ -39,6 +40,7 @@ public class SetInfoService {
         Account  account = accountRepository.findById(request.getAccountId()).orElseThrow(()-> new NotFoundException("존재하지 않는 유저입니다."));
         Exercise exercise = exerciseRepository.findById(request.getExerciseId()).orElseThrow(()-> new NotFoundException("존재하지 않는 운동입니다."));
         Routine routine = routineRepository.findById(request.getRoutineId()).orElseThrow(()-> new NotFoundException("존재하지 않는 루틴입니다."));
+        isContainData(account,routine,exercise);
 
         SetInfo setInfo = SetInfo.builder()
                                 .setNumber(request.getSetNumber())
@@ -52,6 +54,20 @@ public class SetInfoService {
         setInfo = routineSetRepository.save(setInfo);
         SetInfoDto.RegisterResponse response = modelMapper.map(setInfo, SetInfoDto.RegisterResponse.class);
         return response;
+    }
+
+    public void isContainData(Account account, Routine routine, Exercise exercise) throws NotFoundException {
+        RoutineCategory category = routine.getRoutineCategory();
+        List<RoutineCategory> checkCategoryList = account.getRoutineCategoryList();
+        if(checkCategoryList.contains(category) == false){
+            throw new NotFoundException("해당 루틴은 유저정보와 매칭되지 않는 정보입니다.");
+        }
+
+        List<Exercise> checkExerciseList = routine.getExerciseList();
+        if(checkExerciseList.contains(exercise) == false){
+            throw new NotFoundException("해당 운동은 루틴정보와 매칭되지 않는 정보입니다.");
+        }
+
     }
 
     public SetInfoDto.SetInfoResponse getSetInfoCollection(Long accountId, Long routineId, Long exerciseId) {
@@ -68,10 +84,10 @@ public class SetInfoService {
         return response;
     }
 
-   public void updateSetInfo(SetInfoDto.UpdateRequest request) throws NotFoundException {
+   public SetInfoDto.SetInfoResponse updateSetInfo(SetInfoDto.UpdateRequest request) throws NotFoundException {
         SetInfo setInfo = routineSetRepository.findById(request.getSetId()).orElseThrow(() -> new NotFoundException("존재하지 않는 세트입니다."));
         setInfo.updateSetInfo(request);
-        return ;
+        return getSetInfoCollection(setInfo.getAccount().getId(), setInfo.getRoutine().getRoutineId(), setInfo.getExercise().getExerciseId());
    }
 
    public SetInfoDto.SetDetail getSetInfo(Long setId) throws NotFoundException {
@@ -79,16 +95,22 @@ public class SetInfoService {
        return responseBySetInfo(setInfo);
    }
 
-   public void deleteSetInfo(Long setId) throws NotFoundException {
+   public SetInfoDto.SetInfoResponse deleteSetInfo(Long setId) throws NotFoundException {
        SetInfo setInfo = routineSetRepository.findById(setId).orElseThrow(() -> new NotFoundException("존재하지 않는 세트입니다."));
        Integer deletedSetNum = setInfo.getSetNumber();
+       SetInfoDto.SetInfoRequest request = SetInfoDto.SetInfoRequest.builder()
+                                                        .accountId(setInfo.getAccount().getId())
+                                                        .exerciseId(setInfo.getExercise().getExerciseId())
+                                                        .routineId(setInfo.getRoutine().getRoutineId())
+                                                        .build();
 
        routineSetRepository.delete(setInfo);
        List<SetInfo> setInfoList = routineSetRepository.findAllByAccount_IdAndRoutine_RoutineIdAndExercise_ExerciseId(setInfo.getAccount().getId(), setInfo.getRoutine().getRoutineId(), setInfo.getExercise().getExerciseId());
        setInfoList = setInfoList.stream().filter(setInfos -> setInfos.getSetNumber() > deletedSetNum)
                                             .map(info -> info.reduceSetNumber())
                                             .collect(Collectors.toList());
-       return;
+
+       return getSetInfoCollection(request.getAccountId(), request.getRoutineId(), request.getExerciseId());
    }
 
 
