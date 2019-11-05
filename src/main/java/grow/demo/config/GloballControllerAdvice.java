bfadc9636636bc6bac07365c346adfe975def66d
LeeKyuTae <crown0403@khu.ac.kr>
2019-11-05 15:54:12 +0900

    change pring error meaage : e.printstacktrace -> e.getmessage

--- src/main/java/grow/demo/config/GloballControllerAdvice.java
+++ src/main/java/grow/demo/config/GloballControllerAdvice.java
@@ -21,25 +21,31 @@ public class GloballControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({NotFoundException.class})
     public ResponseEntity handleNotFountException(NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
 
+    /*
     // 이게 있어야 하는건지 의문임
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity handleRuntimeException(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("원인 불명 에러" + "\n" + e.getStackTrace());
     }
 
+     */
+
 
     @ExceptionHandler({ClientProtocolException.class})
     public ResponseEntity handleClientProtocolException(ClientProtocolException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler({IOException.class})
     public ResponseEntity handleIOException(IOException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
 
@@ -49,42 +55,50 @@ public class GloballControllerAdvice {
 
     @ExceptionHandler({UnauthorizedException.class})
     public ResponseEntity handleUnauthorizedException(UnauthorizedException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
     @ExceptionHandler({ExistRoutineException.class})
     public ResponseEntity handleExistRoutineException(ExistRoutineException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler({ExistExerciseException.class})
     public ResponseEntity handleExistExerciseException(ExistExerciseException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler({NotExistExerciseException.class})
     public ResponseEntity handleNotExistExerciseException(NotExistExerciseException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler({NotModifyCategoryException.class})
     public ResponseEntity handleNotModifyCategoryException(NotModifyCategoryException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler({ExistUserException.class})
     public ResponseEntity handleExistUserException(ExistUserException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler({ExistCategoryException.class})
     public ResponseEntity handleExistCategoryException(ExistCategoryException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler({ExistRoleException.class})
     public ResponseEntity handleExistRoleException(ExistRoleException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        System.out.println(e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
 }
